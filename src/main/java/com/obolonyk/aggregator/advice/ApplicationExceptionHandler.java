66486273199fb10a646dd0ba6bd8fdc4cf7dca98
U0L9511 <estclio09@gmail.com>
2024-08-04 12:05:06 +0200

    - add controller advice

---
+++ src/main/java/com/obolonyk/aggregator/advice/ApplicationExceptionHandler.java
@@ -0,0 +1,37 @@
+package com.obolonyk.aggregator.advice;
+
+import com.obolonyk.aggregator.exception.CustomerNotFoundException;
+import com.obolonyk.aggregator.exception.InvalidTradeRequestException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.net.URI;
+import java.util.function.Consumer;
+
+@ControllerAdvice
+public class ApplicationExceptionHandler {
+
+    @ExceptionHandler(CustomerNotFoundException.class)
+    public ProblemDetail handleException(CustomerNotFoundException exception){
+        return problemDetailBuilder(HttpStatus.NOT_FOUND, exception, problem -> {
+            problem.setType(URI.create("http://example.com/customer-not-found"));
+            problem.setTitle("Customer not found");
+        } );
+    }
+
+    @ExceptionHandler(InvalidTradeRequestException.class)
+    public ProblemDetail handleException(InvalidTradeRequestException exception){
+        return problemDetailBuilder(HttpStatus.BAD_REQUEST, exception, problem -> {
+            problem.setType(URI.create("http://example.com/invalid-trade-request "));
+            problem.setTitle("Invalid trade request");
+        } );
+    }
+
+    private ProblemDetail problemDetailBuilder(HttpStatus status, Exception ex, Consumer<ProblemDetail> consumer){
+        var problemDetail = ProblemDetail.forStatusAndDetail(status, ex.getMessage());
+        consumer.accept(problemDetail);
+        return problemDetail;
+    }
+}
