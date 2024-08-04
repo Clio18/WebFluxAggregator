9cb9d0de84f8cf230dac5296c717350e59b0689a
U0L9511 <estclio09@gmail.com>
2024-08-04 10:50:52 +0200

    - add cache, retry to enable stock service calling once

--- src/main/java/com/obolonyk/aggregator/client/StockServiceClient.java
+++ src/main/java/com/obolonyk/aggregator/client/StockServiceClient.java
@@ -1,6 +1,7 @@
 package com.obolonyk.aggregator.client;
 
 import com.obolonyk.aggregator.domain.Ticker;
+import com.obolonyk.aggregator.dto.PriceUpdate;
 import com.obolonyk.aggregator.dto.StockPriceResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,10 +9,15 @@ import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
+
+import java.time.Duration;
+import java.util.Objects;
 
 public class StockServiceClient {
     public static final Logger log = LoggerFactory.getLogger(StockServiceClient.class);
     private final WebClient client;
+    private Flux<PriceUpdate> flux;
 
     public StockServiceClient(WebClient client) {
         this.client = client;
@@ -24,13 +30,27 @@ public class StockServiceClient {
                 .bodyToMono(StockPriceResponse.class);
     }
 
-    public Flux<StockPriceResponse> getPriceUpdate() {
+    // getPriceUpdate should be calling only once
+    public Flux<PriceUpdate> priceUpdateStream(){
+        if(Objects.isNull(flux)){
+            flux = getPriceUpdate();
+        }
+        return flux;
+    }
+
+    private Flux<PriceUpdate> getPriceUpdate() {
         return client.get()
                 .uri("/stock/price-stream")
                 .accept(MediaType.APPLICATION_NDJSON)
                 .retrieve()
-                .bodyToFlux(StockPriceResponse.class); 
+                .bodyToFlux(PriceUpdate.class)
+                .retryWhen(retry())
+                .cache(1);
+    }
 
+    private Retry retry(){
+        return Retry.fixedDelay(100, Duration.ofSeconds(1))
+                .doBeforeRetry(rs -> log.info("service failed - {}, retry...", rs.failure().getMessage()));
     }
 }
 
