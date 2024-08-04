3f07ce4b1e68e653cde24794fa9adf1515db26c2
U0L9511 <estclio09@gmail.com>
2024-08-04 15:11:24 +0200

    - add test

--- src/test/java/com/obolonyk/aggregator/CustomerInformationTest.java
+++ src/test/java/com/obolonyk/aggregator/CustomerInformationTest.java
@@ -6,6 +6,8 @@ import org.mockserver.model.HttpResponse;
 import org.mockserver.model.MediaType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 import java.util.Objects;
 
@@ -15,20 +17,47 @@ public class CustomerInformationTest extends AbstractIntegrationTest {
 
     @Test
     public void customerInformation() {
-        // mock customer service
-        var responseBody = resourceToString("customer-service/customer-info-200.json");
+        String relativePath = "customer-service/customer-info-200.json";
+        int statusCode = 200;
+        mockCustomerInfo(relativePath, statusCode);
 
+        getCustomerInfo(HttpStatus.OK)
+                .jsonPath("$.id").isEqualTo(1)
+                .jsonPath("$.name").isEqualTo("sam")
+                .jsonPath("$.balance").isEqualTo(1000)
+                .jsonPath("$.holdings[0].ticker").isEqualTo("GOOGLE")
+                .jsonPath("$.holdings[0].quantity").isEqualTo(2);
+    }
+
+    @Test
+    public void customerNotFound() {
+        String relativePath = "customer-service/customer-info-404.json";
+        int statusCode = 404;
+        mockCustomerInfo(relativePath, statusCode);
+
+        getCustomerInfo(HttpStatus.NOT_FOUND)
+                .jsonPath("$.detail").isEqualTo("Customer with ID 1 not found")
+                .jsonPath("$.title").isNotEmpty();
+    }
+
+    private void mockCustomerInfo(String path, int responseCode ){
+        var responseBody = resourceToString(path);
         mockServerClient.when(HttpRequest.request("/customers/1"))
                 .respond(
                         HttpResponse.response(responseBody)
-                                .withStatusCode(200)
+                                .withStatusCode(responseCode)
                                 .withContentType(MediaType.APPLICATION_JSON  )
                 );
-
-        client.get()
-                .uri("/customers/1")
-                .exchange()
-                .expectBody()
-                .consumeWith(b -> log.info("{}", new String(Objects.requireNonNull(b.getResponseBody()))));
     }
+
+    private WebTestClient.BodyContentSpec getCustomerInfo(HttpStatus expectedStatus){
+         return client.get()
+                 .uri("/customers/1")
+                 .exchange()
+                 .expectStatus().isEqualTo(expectedStatus)
+                 .expectBody()
+                 .consumeWith(b -> log.info("{}", new String(Objects.requireNonNull(b.getResponseBody()))));
+    }
+
+
 }
